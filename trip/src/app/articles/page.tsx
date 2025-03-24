bc7818ad65b878cc9fc602584ed150845d225375
ritukanchi <ritukanchi04@gmail.com>
2025-03-24 23:47:47 +0530

    basic re-org

---
+++ trip/src/app/articles/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { fetchArticle } from "../lib/firestore";
+
+export default function article() {
+  const [article, setArticle] = useState<{ id: string; title: string; content: string } | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchArticle();
+      setArticle(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <h1>Article</h1>
+      {article ? (
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </div>
+      ) : (
+        <p>Loading or no article found.</p>
+      )}
+    </main>
+  );
+}
+
--- trip/src/app/page.tsx
+++ trip/src/app/page.tsx
@@ -1,30 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { fetchArticle } from "../app/lib/firestore";
+import Link from "next/link";
 
 export default function Home() {
-  const [article, setArticle] = useState<{ id: string; title: string; content: string } | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchArticle();
-      setArticle(data);
-    };
-    fetchData();
-  }, []);
-
   return (
-    <main>
-      <h1>Article</h1>
-      {article ? (
-        <div>
-          <h2>{article.title}</h2>
-          <p>{article.content}</p>
-        </div>
-      ) : (
-        <p>Loading or no article found.</p>
-      )}
-    </main>
+    <div>
+      <Link href="/articles">Go to Article</Link>
+    </div>
   );
 }
+
