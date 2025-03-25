26985a85cfe6602f5bec00bbd7097380ce007a2c
Harshit <79516676+Harshxt@users.noreply.github.com>
2025-03-26 00:30:44 +0530

    Added component.css and worked login

--- trip/src/app/components/login/page.tsx
+++ trip/src/app/components/login/page.tsx
@@ -1,10 +1,31 @@
-"use client"; 
+"use client";
+
+import "../component.css";
 export default function login() {
-    return (
-      <div>
-        <h2>Log in!</h2>
-        <p></p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="loginContainer">
+      <h2>LOGIN</h2>
+      <form >
+        <label>Email Account</label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          required
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          required
+        />
+        <a href="./signup" className="link">Don't have an account?</a>
+
+        <button type="submit">LOGIN</button>
+
+      </form>
+
+      <p></p>
+    </div>
+  );
+}
