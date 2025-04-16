3fe377a44282173e64b5791e6d017d9f4483dbf3
Harshit <79516676+Harshxt@users.noreply.github.com>
2025-04-16 12:32:54 +0530

    Added destination page

--- trip/src/app/components/destination/page.tsx
+++ trip/src/app/components/destination/page.tsx
@@ -1,9 +1,40 @@
 "use client"; 
-export default function destination() {
-    return (
-      <div>
-        <h2>Top destinations!</h2>
+import Image from "next/image";
+import "./destination.css";
+
+export default function Destination() {
+  return (
+    <div className="destination-container">
+      <h2 className="destination-title">Top destinations!</h2>
+      <div className="destination-grid">
+        <div className="destination-item">
+          <Image
+            src="/images/jaipur.png"
+            alt="Jaipur"
+            
+            className="destination-image"
+          />
+          <p className="destination-name">JAIPUR</p>
+        </div>
+        <div className="destination-item">
+          <Image
+            src="/images/goa.png"
+            alt="Goa"
+            
+            className="destination-image"
+          />
+          <p className="destination-name">GOA</p>
+        </div>
+        <div className="destination-item">
+          <Image
+            src="/images/delhi.png"
+            alt="Delhi"
+            
+            className="destination-image"
+          />
+          <p className="destination-name">DELHI</p>
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
